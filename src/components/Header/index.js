644355ae2146c8919529637e72687b184df0e619
Luis Felipe <1luisfelipe2@gmail.com>
2022-04-14 17:12:48 -0400

    add shareButton

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,10 @@
-import { AppBar, CssBaseline, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import "./style.css";
 
@@ -7,6 +13,25 @@ export default function Header(props) {
   const date = `${current.getDate()}/${
     current.getMonth() + 1
   }/${current.getFullYear()} às ${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`;
+
+  const navigatorHasShare = navigator.share;
+
+  const textCovid19 = `Painel Covid19`;
+
+  const shareInfo = () => {
+    navigator.share({
+      title: `Dados do Covid 19`,
+      text: textCovid19,
+      url: "/",
+    });
+  };
+  const renderShareButton = (
+    <div>
+      <Button variant="contained" color="primary" onClick={shareInfo}>
+        Compartilhar
+      </Button>
+    </div>
+  );
 
   return (
     <>
@@ -17,6 +42,8 @@ export default function Header(props) {
             Painel Covid-19 {props.pais}
             <br />
             Atualizado em: {date}
+            <br />
+            {navigatorHasShare ? renderShareButton : null}
           </Typography>
         </Toolbar>
       </AppBar>
